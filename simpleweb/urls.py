c2b54b5d31579949c1966a438a8c48622d9ef841
Brandon N <Brandon.njoroge19@gmail.com>
2020-05-05 19:15:08 +0300

    Removed success page

--- simpleweb/urls.py
+++ simpleweb/urls.py
@@ -4,5 +4,4 @@ from .import views
 urlpatterns = [
     path('', views.HomeView, name='home'),
     path('contact/', views.ContactView, name='contact'),
-    path('contact/success/', views.SuccessView, name='contact-success')
 ]
